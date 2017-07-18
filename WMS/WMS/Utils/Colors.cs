fcb5efa14293c13ed2ad9ae374ad407642043032
Shehzad Bilal <shehzad.bilal@10pearls.com>
2017-07-18 15:51:39 +0500

    Initial Template Commit

---
+++ WMS/WMS/Utils/Colors.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WMS
+{
+    public class Colors
+    {
+        public static string PrimaryColor = "#1F3664";
+
+    }
+}
---
+++ WMS/WMS/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WMS.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public BaseViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string property) {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}

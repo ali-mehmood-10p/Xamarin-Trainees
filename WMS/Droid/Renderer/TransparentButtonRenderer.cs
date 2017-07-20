6aae3c1f1103d1222105d8edf85ce4581ec94482
Shehzad Bilal <shehzad.bilal@10pearls.com>
2017-07-20 16:42:02 +0500

    Changes to Layout

---
+++ WMS/Droid/Renderer/TransparentButtonRenderer.cs
@@ -0,0 +1,17 @@
+﻿using Android.Graphics;
+using WMS;
+using WMS.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(TransparentButton), typeof(TransparentButtonRenderer))]
+namespace WMS.Droid
+{
+    public class TransparentButtonRenderer : ButtonRenderer
+	{
+        public override void Draw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+        }
+    }
+}
